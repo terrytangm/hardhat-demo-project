9c8b39a14e71bf271ed8dbaccab389d6caf16b56
Terry Tang <mtang@realfeng.com>
2023-06-03 00:46:10 +0800

    commented out claimtoken.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,43 +1,46 @@
 // require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-waffle");
-require("./tasks/claimtoken");
+//require("./tasks/claimtoken");
 require("./tasks/eth_foxe_getmerkle");
-require("./tasks/generateMerkle")
+require("./tasks/generateMerkle");
 require("./tasks/taskdemo");
 require("./tasks/account_utils");
 // 配置gas估算插件
 require("hardhat-gas-reporter");
 require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
-const {projectId, mnemonic1,ALCHEMY_API_KEY} = process.env
+const { projectId, mnemonic1, ALCHEMY_API_KEY } = process.env;
 module.exports = {
   networks: {
     // npx hardhat run scripts/deploy.js --network eth_testnet
     // 领水地址 : https://goerlifaucet.com/
     eth_testnet: {
-      url: `https://goerli.infura.io/v3/${projectId}`,  
+      url: `https://goerli.infura.io/v3/${projectId}`,
       accounts: {
-        mnemonic: "obscure satoshi lecture culture lady pattern fog shoe emerge step wonder sword"
-      }
+        mnemonic:
+          "obscure satoshi lecture culture lady pattern fog shoe emerge step wonder sword",
+      },
     },
     bsc: {
       url: `https://bsc-dataseed.binance.org/`,
       accounts: {
-        mnemonic: "obscure satoshi lecture culture lady pattern fog shoe emerge step wonder sword"
-      }
+        mnemonic:
+          "obscure satoshi lecture culture lady pattern fog shoe emerge step wonder sword",
+      },
     },
     bsc_testnet: {
       url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
       accounts: {
-        mnemonic: "obscure satoshi lecture culture lady pattern fog shoe emerge step wonder sword"
-      }
+        mnemonic:
+          "obscure satoshi lecture culture lady pattern fog shoe emerge step wonder sword",
+      },
     },
     hardhat: {
       forking: {
         url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-        blockNumber: 17342219
-      }
-    }
+        blockNumber: 17342219,
+      },
+    },
   },
   solidity: {
     // 可修改编译器版本
@@ -58,9 +61,9 @@ module.exports = {
   },
   gasReporter: {
     // 开启gas估算插件 设置人民币和gas费换算，也可以设置为美元 usd
-  	enabled: false,
-    currency: 'USD',
+    enabled: false,
+    currency: "USD",
     token: "ETH",
-    coinmarketcap:process.env.COINMARKETCAP_API_KEY,
-  }
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
 };
